da2fdc96bc6baddd53f7045cd22dad14a8835f96
73031584 <73031584@continental.edu.pe>
2024-09-02 16:46:03 -0500

    Implementación del Máximo Común Divisor (MCD)

---
+++ numeroEntero.py
@@ -0,0 +1,18 @@
+class numeroEntero:
+
+    def MCD(self,a,b):
+        x = abs(a)
+        y = abs(b)
+
+        while y!=0:
+            remainder = x%y
+            x=y
+            y=remainder
+        return x
+
+if __name__ == '__main__':
+    numero1=int(input("Primer número: "))
+    numero2=int(input("Segundo número: "))
+
+    operacion = numeroEntero()
+    print(f"MCD de {numero1} y {numero2} es {operacion.MCD(numero1,numero2)}")
